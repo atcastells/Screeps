0f1b49490310f8181698b9f8841ae4eab7d15072
atcastells <aron.deltebre@gmail.com>
2016-07-03 12:42:55 +0200

    Architect memory

--- role.architect.js
+++ role.architect.js
@@ -1,19 +1,24 @@
 /**
  * Created by acastells on 03/07/2016.
  */
-var stage = 0;
-var containers = 0;
-var extensions = 0;
 var roleArchitect = {
 
     /*Initial constructions*/
 
     run: function (creep) {
-        /*Check room*/
-        var buildings = creep.room.find(FIND_MY_STRUCTURES, {
+        /*Check rooms*/
+        Memory.architectLog = {
+            roomHash: Memory.roomName
+        };
+        var containers = creep.room.find(FIND_MY_STRUCTURES, {
             filter: (structure) => {
-                return (structure.structureType == STRUCTURE_CONTAINER ||
-                structure.structureType == STRUCTURE_EXTENSION)
+                return (structure.structureType == STRUCTURE_CONTAINER)
+            }})
+
+
+        var spawn = creep.room.find(FIND_MY_STRUCTURES, {
+            filter: (structure) => {
+                return (structure.structureType == STRUCTURE_EXTENSION)
             }})
 
         if(stage == 1){
