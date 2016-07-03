3f10d061a3479ff3db466a006cb404b01b7ceed5
atcastells <aron.deltebre@gmail.com>
2016-07-03 11:37:02 +0200

    Harvester functionality without hauler.

--- role.harvester.js
+++ role.harvester.js
@@ -12,7 +12,25 @@ var roleHarvester = {
             }
         }
         else {
-            creep.drop(RESOURCE_ENERGY);
+            if(creep.room.find(FIND_CREEPS, {
+                    filter: (creep) => {
+                        return (creep.memory.role == 'hauler');
+                    }
+                })){
+                creep.drop(RESOURCE_ENERGY);
+            }
+            else {
+                var storage = creep.room.find(FIND_MY_STRUCTURES, {
+                    filter: (structure) =>{
+                        return (structure.structureType == STRUCTURE_SPAWN ||
+                        structure.structureType == STRUCTURE_CONTAINER);
+                    }
+                });
+                var nearestStorage = creep.pos.findClosestByPath(storage);
+             if(creep.transfer(nearestStorage) == ERR_NOT_IN_RANGE){
+                 creep.moveTo(nearestStorage);
+             }
+            }
         }
     }
 };
