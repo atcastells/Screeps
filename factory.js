b5b11f344a2edf9d3eaf7ff8ecca8c9142aa5101
atcastells <aron.deltebre@gmail.com>
2016-07-03 20:09:07 +0200

    Factory preparation

--- factory.js
+++ factory.js
@@ -6,34 +6,31 @@ var factory = {
     normalSpawn: function (maxOrganizers,maxBuilders,maxHaulers,maxUpgraders,maxHarvesters) {
         var organizers = _.filter(Game.creeps, (creep) => creep.memory.role == 'organizer');
         if(organizers.length < maxOrganizers){
-            if (!(Game.spawns.Spawn1.createCreep([MOVE], undefined, {role: 'organizer'}) == ERR_NOT_ENOUGH_ENERGY)){
+            if (!(Game.spawns.Spawn1.createCreep([MOVE], undefined, {role: 'organizer'}) == (ERR_NOT_ENOUGH_ENERGY || ERR_BUSY))){
                 console.log('organizer Spawned');
             }
         }
         var builders = _.filter(Game.creeps, (creep) => creep.memory.role == 'builder');
         if(builders.length < maxBuilders){
-            if (!(Game.spawns.Spawn1.createCreep([CARRY,WORK,MOVE], undefined, {role: 'builder'}) == ERR_NOT_ENOUGH_ENERGY)) {
+            if (!(Game.spawns.Spawn1.createCreep([CARRY,WORK,MOVE], undefined, {role: 'builder'}) == (ERR_NOT_ENOUGH_ENERGY || ERR_BUSY))) {
                 console.log('builder Spawned');
             }
         }
-
-
         var haulers = _.filter(Game.creeps, (creep) => creep.memory.role == 'hauler');
         if(haulers.length < maxHaulers){
-            if (!(Game.spawns.Spawn1.createCreep([CARRY,CARRY,MOVE], undefined, {role: 'hauler'}) == ERR_NOT_ENOUGH_ENERGY)) {
+            if (!(Game.spawns.Spawn1.createCreep([CARRY,CARRY,MOVE], undefined, {role: 'hauler'}) == (ERR_NOT_ENOUGH_ENERGY || ERR_BUSY))) {
                 console.log('hauler Spawned');
             }
         }
-
         var upgraders = _.filter(Game.creeps, (creep) => creep.memory.role == 'upgrader');
         if(upgraders.length < maxUpgraders) {
-            if (!(Game.spawns.Spawn1.createCreep([CARRY, CARRY, MOVE], undefined, {role: 'upgrader'}) == ERR_NOT_ENOUGH_ENERGY)) {
+            if (!(Game.spawns.Spawn1.createCreep([CARRY, CARRY, MOVE], undefined, {role: 'upgrader'}) == (ERR_NOT_ENOUGH_ENERGY || ERR_BUSY))) {
                 console.log('upgrader Spawned');
             }
         }
         var harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester');
         if(harvesters.length < maxHarvesters) {
-            if (!(Game.spawns.Spawn1.createCreep([CARRY, WORK, MOVE], undefined, {role: 'harvester'}) == ERR_NOT_ENOUGH_ENERGY)) {
+            if (!(Game.spawns.Spawn1.createCreep([CARRY, WORK, MOVE], undefined, {role: 'harvester'}) == (ERR_NOT_ENOUGH_ENERGY || ERR_BUSY))) {
                 console.log('harvester Spawned');
             }
         }
