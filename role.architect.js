fdfa577891f1af4bb0564e959e969c9b9855bced
atcastells <aron.deltebre@gmail.com>
2016-07-03 13:08:49 +0200

    Architect memory

--- role.architect.js
+++ role.architect.js
@@ -7,9 +7,34 @@ var roleArchitect = {
 
     run: function (creep) {
         /*Check rooms*/
-        Memory.architectLog = {
-            roomHash: Memory.roomName
+
+        for(var id in Game.rooms){
+            var room = Game.rooms[id];
+            if(!Memory.rooms[room.name]){
+                Memory.rooms[room.name]={};
+
+                var sources = room.find(FIND_SOURCES);
+                var buildings = room.find(FIND_MY_STRUCTURES);
+                for(var x in sources) {
+                    var path = Game.spawns.Spawn1.pos.findPathTo(sources[x]);
+
+                    for(var i in path) {
+                        Game.rooms.sim.createConstructionSite(path[i].x, path[ i].y, STRUCTURE_ROAD);
+                    }
+                }
+                Memory.architectLog.Projects=[];
+                Memory.architectLog.Structures=[];
+                Memory.architectLoc.Structures.push({id: buildings[ids]});
+                Memory.rooms[room.name].sources=[];
+                for(var ids in sources){
+                    var klair = sources[ids].pos.findInRange(FIND_STRUCTURES, 6, {filter: { structureType: STRUCTURE_KEEPER_LAIR }}).length > 0;
+                    Memory.rooms[room.name].sources.push({id: sources[ids].id ,slots: 3, klair: klair});
+                };
+            };
         };
+
+
+
         var containers = creep.room.find(FIND_MY_STRUCTURES, {
             filter: (structure) => {
                 return (structure.structureType == STRUCTURE_CONTAINER)
