f8db2c8c0bb180b86174dbba71c76f7d565dee72
atcastells <aron.deltebre@gmail.com>
2016-07-03 19:58:10 +0200

    Factory preparation

--- factory.js
+++ factory.js
@@ -5,19 +5,36 @@ var factory = {
 
     normalSpawn: function (maxOrganizers,maxBuilders,maxHaulers,maxUpgraders,maxHarvesters) {
         var organizers = _.filter(Game.creeps, (creep) => creep.memory.role == 'organizer');
-        if(organizers.length < maxOrganizers) var newName = Game.spawns.Spawn1.createCreep([MOVE], undefined, {role: 'organizer'});
+        if(organizers.length < maxOrganizers){
+            var newName = Game.spawns.Spawn1.createCreep([MOVE], undefined, {role: 'organizer'});
+            console.log('organizer Spawned');
+        }
+
 
         var builders = _.filter(Game.creeps, (creep) => creep.memory.role == 'builder');
-        if(builders.length < maxBuilders) var newName = Game.spawns.Spawn1.createCreep([CARRY,WORK,MOVE], undefined, {role: 'builder'});
+        if(builders.length < maxBuilders){
+            var newName = Game.spawns.Spawn1.createCreep([CARRY,WORK,MOVE], undefined, {role: 'builder'});
+            console.log('builder Spawned');
+        }
+
 
         var haulers = _.filter(Game.creeps, (creep) => creep.memory.role == 'hauler');
-        if(haulers.length < maxHaulers) var newName = Game.spawns.Spawn1.createCreep([CARRY,CARRY,MOVE], undefined, {role: 'hauler'});
+        if(haulers.length < maxHaulers){
+            var newName = Game.spawns.Spawn1.createCreep([CARRY,CARRY,MOVE], undefined, {role: 'hauler'});
+            console.log('hauler Spawned');
+        }
 
         var upgraders = _.filter(Game.creeps, (creep) => creep.memory.role == 'upgrader');
-        if(upgraders.length < maxUpgraders) var newName = Game.spawns.Spawn1.createCreep([CARRY,CARRY,MOVE], undefined, {role: 'upgrader'});
+        if(upgraders.length < maxUpgraders){
+            var newName = Game.spawns.Spawn1.createCreep([CARRY,CARRY,MOVE], undefined, {role: 'upgrader'});
+            console.log('upgrader Spawned');
+        }
 
         var harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester');
-        if(harvesters.length < maxHarvesters) var newName = Game.spawns.Spawn1.createCreep([CARRY,WORK,MOVE], undefined, {role: 'harvester'});
+        if(harvesters.length < maxHarvesters) {
+            var newName = Game.spawns.Spawn1.createCreep([CARRY,WORK,MOVE], undefined, {role: 'harvester'});
+            console.log('harvester Spawned');
+        };
 
     }
     /*Populate lists and spawn*/
