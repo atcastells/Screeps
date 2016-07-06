9fffbff8801f808cad7d651adeea5ac2049f9c36
atcastells <aron.deltebre@gmail.com>
2016-07-06 22:51:57 +0200

    Difference function

--- utils.js
+++ utils.js
@@ -6,20 +6,20 @@ var utils = {
         var source = creep.room.find(FIND_STRUCTURES, {
             filter: (structure) => {
                 return (structure.structureType == STRUCTURE_SPAWN ||
-                    structure.structureType == STRUCTURE_CONTAINER) && structure.energy >= (structure.energyCapacity/2);
+                    structure.structureType == STRUCTURE_CONTAINER) && structure.energy >= (structure.energyCapacity / 2);
             }
         });
         var targetStructure = creep.pos.findClosestByRange(source);
         targetStructure.transferEnergy(creep);
     },
-    GetCreepsByRole: function (role){
-        var CreepList = [];
-        for (var creepname in Game.creeps){
-            if (Game.creeps[creepname].memory.role == role){
-                CreepList.push(Game.creeps[creepname]);
+    difference: function (a1, a2) {
+        var result = [];
+        for (var i = 0; i < a1.length; i++) {
+            if (a2.indexOf(a1[i]) === -1) {
+                result.push(a1[i]);
             }
         }
-        return CreepList
+        return result;
     }
 };
 module.exports = utils;
