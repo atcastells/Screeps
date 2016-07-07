1486d680fddebc3455a193726415438c0238ea5b
atcastells <aron.deltebre@gmail.com>
2016-07-07 23:03:19 +0200

    Update role.builder.js

--- role.builder.js
+++ role.builder.js
@@ -2,39 +2,17 @@ var utils = require('utils');
 var roleBuilder = {
 
     /** @param {Creep} creep **/
-    run: function(creep) {
-
-        if(creep.memory.building && creep.carry.energy == 0) {
-            creep.memory.building = false;
-        }
-        if(!creep.memory.building && creep.carry.energy == creep.carryCapacity) {
-            creep.memory.building = true;
-        }
-
-        if(creep.memory.building) {
-            var targets = creep.room.find(FIND_CONSTRUCTION_SITES);
-            if(targets.length) {
-                if(creep.build(targets[0]) == ERR_NOT_IN_RANGE) {
-                    creep.moveTo(targets[0]);
-                }
+    run: function (creep) {
+        var project = 0;
+        var target;
+        for (var i in Game.rooms) {
+            var room = Game.rooms[i];
+            if (!Memory.creeps[creep.name].working) {
+                Memory.creeps[creep.name].working = false;
+                Memory.creeps[creep.name].taskAssigned = false;
             }
-        }
-        else {
-            var sources = creep.room.find(FIND_STRUCTURES, {
-                filter: (structure) => {
-                    return (structure.structureType == STRUCTURE_SPAWN ||
-                        structure.structureType == STRUCTURE_CONTAINER) && 
-                        structure.energy >= (structure.energyCapacity/2);
-                }
-            });
-            var path = creep.pos.findClosestByPath(sources);
-            creep.moveTo(path);
-            if(creep.pos.getRangeTo(path) == 1 && creep.carry.energy < creep.carryCapacity){
-                creep.say('Filling');
-                utils.fillCreep(creep);
-            };
+            /*Create work project*/
         }
     }
 };
-
 module.exports = roleBuilder;
